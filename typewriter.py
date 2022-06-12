b7a6a34c1658d710dadc79bb6b3c411d150c470e
01-turgut <gilgilturgut@gmail.com>
2022-06-12 09:30:33 +0000

    Created file for typewriter effect and created typewriter function

---
+++ typewriter.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+
+def typewriter(message):
+    '''Typewriter will help me to print my story letter by letter'''
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+
+        if char != "\n":
+            time.sleep(0.1)
+        else:
+            time.sleep(1)
