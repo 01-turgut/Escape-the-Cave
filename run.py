9151d5ad321d4f8010a22bca8eecc2062838bda0
01-turgut <gilgilturgut@gmail.com>
2022-06-12 09:51:00 +0000

    Import typewriter.py to run.py file and import the typewrite function to run.py

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import colorama
+from colorama import Fore
+import typewriter
+from typewriter import typewriter
+
+
+Messae = Fore.RED + "Hello World\n"
+typewriter(Messae)
