947dff331f6b7dcbf0c7d4e40ee2a857f22fecce
01-turgut <gilgilturgut@gmail.com>
2022-06-13 19:24:32 +0000

    Create banner.py file to create a function that creates banner for the game

--- run.py
+++ run.py
@@ -4,10 +4,31 @@ from colorama import Fore, Back
 from typewriter import typewriter
 from banner import banner
 
+SEC = 1
+TXTCLR = Back.RED + Fore.WHITE
+RESET = Back.RESET + Fore.RESET
+
+def beginning():
+
+
+def start():
+    """This function is starting the game"""
+    question1 = input("Would you like to start the game? (Y/N): ")
+    if question1 == "N" or question1 == "n":
+        print()
+        typewriter(TXTCLR + "Go home coward! \n" + RESET)
+    elif question1 == "Y" or question1 == "y":
+        beginning()
+    else:
+        print(Fore.RED + "Invalid Entry, Wait for game to load again...\n" + Fore.RESET)
+        time.sleep(SEC)
+        banner()
+        time.sleep(4)
+        return start()
+
 
 banner()
+time.sleep(2)
+start()
 
-start = input
-time.sleep(3)
-typewriter(Back.RED + Fore.WHITE + "...You were walking around and saw a cave...\n")
-
+# typewriter("You were walking around and saw a cave\n" + Back.RESET)
